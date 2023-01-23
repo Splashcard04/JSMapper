d309fd7e13d80ebab51e8446336c3fd14a077f25
splashcard <111317032+Splashcard04@users.noreply.github.com>
2023-01-23 21:03:31 +0000

    note class

--- src/v2/JSMV2.js
+++ src/v2/JSMV2.js
@@ -37,7 +37,24 @@ class Note {
     { 
         position: [0, 0],
         type: 0, 
-        cutDirection: 0
+        cutDirection: 0,
+        rotation: [0, 0, 0],
+        localRotation: [0, 0, 0],
+        njs: 8,
+        timeOffset: 0,
+        fake: false,
+        interactable: true,
+        color: [1, 1, 1, 1],
+        disableSpawnEffect: false,
+        track: "track",
+
+        animateColor: [[1, 1, 1, 1, 1]],
+        animateDissolve: [[0, 0]],
+        animateDissolveArrow: [[0, 0]],
+        animateScale: [[1, 1, 1, 1]],
+        animatePosition: [[0, 0, 0, 0]],
+        animateLocalRotation: [[0, 0, 0, 1]],
+        animateDefinitePosition: [[0, 0, 0, 0]]
     }) {
         if(!time) { this.time = 0 } else { this._time = time }
         let pos
@@ -46,7 +63,17 @@ class Note {
         if(!modifications.cutDirection) { this._cutDirection = 0 } else { this._cutDirection = modifications.cutDirection }
 
 
-        this._customData = { _position: pos}
+        this._customData = { _position: pos, _rotation: modifications.rotation, _localRotation: modifications.localRotation,
+            _noteJumpMovementSpeed: modifications.njs, _noteJumpStartBeatOffset: modifications.timeOffset,
+            _fake: modifications.njs, _interactable: modifications.interactable, _color: modifications.color,
+            _disableSpawnEffect: modifications.disableSpawnEffect, _track: modifications.track,
+            _animation: {
+                _color: modifications.animateColor, _dissolve: modifications.animateDissolve,
+                _dissolveArrow: modifications.animateDissolveArrow, _scale: modifications.animateScale,
+                _position: modifications.animatePosition, _localRotation: modifications.animateLocalRotation,
+                _definitePosition: modifications.animateDefinitePosition
+            }
+        }
     }
 
     /** push the note to the difficulty file */
