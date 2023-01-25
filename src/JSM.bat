a8d7057278e3d369b9eff94a5e33f5a414d7f530
splashcard <111317032+Splashcard04@users.noreply.github.com>
2023-01-25 10:11:18 -0800

    comments

--- src/JSM.bat
+++ src/JSM.bat
@@ -1,5 +1,7 @@
-@ echo off
-npm i -g nodemon # checks for updates each time on run
-npm i splashcard_jsmapper
+@ ECHO off
+:: this will check for updates in nodemon and jsm on startup, meaning that re installing the packages with npm is unnecesary
+npm i -g nodemon :: check for update
+npm i splashcard_jsmapper :: check for update
 
+:: nodemon will handel the automatic reload so that npm installs are only run when the batch is first run
 nodemon ./script.js
