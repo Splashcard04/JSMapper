ee2af1339cae37c6c39955b912c638a6b7984d78
splashcard <111317032+Splashcard04@users.noreply.github.com>
2023-01-26 08:23:35 -0800

    jsm label

--- src/JSM.bat
+++ src/JSM.bat
@@ -3,10 +3,13 @@ mode con: cols=65 lines=50
 
 echo Starting ReLoad
 
+::logs for starting script, and ending script
 :loop
-echo [96mStarting Script ReLoad[0m
+echo [96m[JSMapper]: Starting Script ReLoad[0m
 node script.js
-echo [32mScript ReLoad Finished [0m
+echo [32m[JSMapper]:Script ReLoad Finished [0m
 
+:: pause the loop (makes the user press a key)
 pause
+:: loop again after key is pressed
 goto loop
